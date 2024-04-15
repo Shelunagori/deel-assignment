5c1213ca9554303d0a064e299ba6ab2290505347
Shelunagori <shelunagori@gmail.com>
2024-04-15 12:37:18 +0530

    Get all contracts

--- src/app.js
+++ src/app.js
@@ -1,28 +1,42 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const { Op } = require("sequelize");
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { sequelize } = require("./model");
+const { getProfile } = require("./middleware/getProfile");
 const app = express();
 app.use(bodyParser.json());
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
+app.set("sequelize", sequelize);
+app.set("models", sequelize.models);
 
 /**
  * FIX ME!
  * @returns contract by id
  */
 app.get("/contracts/:id", getProfile, async (req, res) => {
+  const { Contract } = req.app.get("models");
+  const profile_id = req.get("profile_id");
+  const { id } = req.params;
+  const contract = await Contract.findOne({
+    where: {
+      id: id,
+      [Op.or]: [{ ClientId: profile_id }, { ContractorId: profile_id }],
+    },
+  });
+  if (!contract) return res.status(404).end();
+  res.json(contract);
+});
+
+app.get("/contracts", getProfile, async (req, res) => {
     const { Contract } = req.app.get("models");
     const profile_id = req.get("profile_id");
-    const { id } = req.params;
-    const contract = await Contract.findOne({
+    const contract = await Contract.findAll({
       where: {
-        id: id,
+        status: { [Op.ne]: "terminated" },
         [Op.or]: [{ ClientId: profile_id }, { ContractorId: profile_id }],
       },
     });
     if (!contract) return res.status(404).end();
     res.json(contract);
-  });
+});
+
 module.exports = app;
