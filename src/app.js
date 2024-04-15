ff466d7c20c211737b5af0ab0c2f2129dda1a492
Shelunagori <shelunagori@gmail.com>
2024-04-15 12:30:26 +0530

    Get Contracts by id fixed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Op } = require("sequelize");
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
 const app = express();
@@ -11,11 +12,17 @@ app.set('models', sequelize.models)
  * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+app.get("/contracts/:id", getProfile, async (req, res) => {
+    const { Contract } = req.app.get("models");
+    const profile_id = req.get("profile_id");
+    const { id } = req.params;
+    const contract = await Contract.findOne({
+      where: {
+        id: id,
+        [Op.or]: [{ ClientId: profile_id }, { ContractorId: profile_id }],
+      },
+    });
+    if (!contract) return res.status(404).end();
+    res.json(contract);
+  });
 module.exports = app;
